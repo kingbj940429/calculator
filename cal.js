897b3c6f6a41d8cbc3005d80ad8f6be2e559ed3b
kingbj940429 <kingbj0429@gmail.com>
2020-09-06 16:16:42 +0900

    add remove

--- cal.js
+++ cal.js
@@ -2,30 +2,36 @@ var number = $('.numbers div');
 var operator = $('.operators div');
 var input = document.getElementById('input');
 var resultDisplayed = false;
-
+var note = document.getElementById('note');
+var resultString;
 //숫자키 클릭시
 for (var i = 0; i < number.length; i++) {
     number[i].addEventListener("click", (e) => {
         console.log(e.target.textContent);
 
-        $('#note').append(e.target.textContent); //메모
-
-
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+        var currentString = input.textContent;
+        var lastChar = currentString[currentString.length - 1]; //
 
         if (resultDisplayed === false) {
             $('#input').append(e.target.textContent); //숫자 입력시 input에 보여줌
         } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
             //equl로 답을 구하고 바로 사칙부호를 넣어서 계속해서 계산할 수 있게 해줌.
-            resultDisplayed = false;//equl로 인해 플래그가 true가 되었으니 다 false로 바꿔줌.
+            resultDisplayed = false; //equl로 인해 플래그가 true가 되었으니 다 false로 바꿔줌.
             input.innerHTML += e.target.innerHTML;
         } else {
             resultDisplayed = false;
             input.innerHTML = "";
             input.innerHTML += e.target.innerHTML;
+           
         }
+        resultString += e.target.textContent;
+       // $('#note').append(e.target.textContent); //메모
 
+       $('#remove').on('click',(e)=>{
+        alert('삭제');
+        currentString += currentString.substring(0,currentString[currentString.length - 1] );
+    })
+      
     });
 }
 
@@ -34,12 +40,28 @@ for (var i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", (e) => {
         console.log(e.target.textContent);
         //$('#input').append(e.target.textContent); //사칙부호 입력시 input에 보여줌
-       
-         $('#note').append(e.target.textContent); //메모
+        var currentString = input.textContent;
+        var lastChar = currentString[currentString.length - 1];
 
+      
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            var newString = currentString.substring(0, currentString.length - 1) + e.target.textContent;
+            //새로 입력되는 사칙연산 전까지의 string에 새롭게 입력되는 문자를 다시 입력한다.
+            input.textContent = newString;
+            note.textContent = newString;//메모장도 사칙부호가 바뀌면 내용이 바뀐다.
+            
+           
+        } else if (currentString.length == 0) {
+            alert('숫자 먼저 입력해주세요');
+        } else {
+            input.textContent += e.target.textContent;
+           // $('#note').append(e.target.textContent); //메모
+        }
+        $('#remove').on('click',(e)=>{
+            alert('삭제');
+        });
+        
        
-
-        input.innerHTML += e.target.innerHTML;
     });
 }
 
@@ -84,14 +106,19 @@ $('#result').on('click', () => {
 
     input.innerHTML = numbers[0];
 
-    $('#note').append(' = <strong>' + numbers[0]+'</strong><br>'+numbers[0]);
+    $('#note').append(inputString +' = <strong>' + numbers[0] + '</strong><br>');
+    if(inputString.length ==0){
+        alert('숫자를 입력해주세요');
+        //$('#note').text('');
+    }
     resultDisplayed = true;
 })
+
+//삭제시
 
 
 
 //clear 클릭시
 $('#clear').on('click', () => {
     $('#input').html('');
-    $('#note').html('');
 })
